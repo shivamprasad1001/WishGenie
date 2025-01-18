e3496294942a2851ac8ef2baafea675f7f9a8304
Shivam Prasad <161421872+shivamprasad1001@users.noreply.github.com>
2025-01-18 17:33:20 +0530

    Update db_config.py

--- db_config.py
+++ db_config.py
@@ -1,4 +1,9 @@
 import pymysql
 
 def get_db_connection():
-    return pymysql.connect("postgresql://wishes_owner:wKb7yjx0UoXn@ep-shiny-forest-a83o2j93-pooler.eastus2.azure.neon.tech/wishes?sslmode=require")
+    return pymysql.connect(
+        host="localhost",
+        user="root",
+        password="password", #your MySQL password
+        database="wishes_db" #you database name 
+    )
